7b69d5d1b71f990c6d399d80e54a8a521f604631
Rigo <217056768+Recsaur@users.noreply.github.com>
2026-08-29 14:03:28 +0400

    Added cheese and particle effects when touching it.

---
+++ cheese.gd
@@ -0,0 +1,19 @@
+extends Area2D
+var collectfx = preload("res://scenes/collectparticle.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	var collectparticle = collectfx.instantiate()
+	get_parent().add_child(collectparticle)
+	collectparticle.position = position
+	GameController.emit_signal("Action",10)
+	queue_free()
